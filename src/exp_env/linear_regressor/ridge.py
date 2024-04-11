07b6c88c852708bf633184961eec4151a6e1896d
yama-yeah <b1021091@fun.ac.jp>
2024-04-11 12:21:26 +0900

    ridgeの実装を追加

---
+++ src/exp_env/linear_regressor/ridge.py
@@ -0,0 +1,59 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+from exp_env.linear_regressor.base import BaseLinearModel
+
+class LazyRidge(nn.Module,BaseLinearModel):
+    def __init__(self, out_features,alpha=1.0,useGradientDescent=False):
+        super().__init__()
+        self.alpha = alpha
+        self.linear = nn.LazyLinear(out_features=out_features)
+        self.linear.weight.requires_grad = useGradientDescent
+        self.useGradientDescent = useGradientDescent
+    
+    def fit(self, X, y):
+        if self.useGradientDescent:
+            raise Exception('if you wanna use gradient descent, please use torch.optim')
+        with torch.no_grad():
+            X=torch.cat([X,torch.ones(X.shape[0],1)],dim=1)
+            I=torch.eye(X.shape[1])
+            lambdaI=self.alpha*I
+            WB=torch.inverse(X.T@X+lambdaI)@X.T@y
+            self.linear.weight.data=WB[:-1]
+            self.linear.bias.data=WB[-1]
+            del WB,I,lambdaI
+            torch.cuda.empty_cache()
+    
+    def loss(self, y_hat, y):
+        return F.mse_loss(y_hat, y) + self.alpha * F.mse_loss(self.linear.weight, torch.zeros_like(self.linear.weight))
+
+    def forward(self, x):
+        return self.linear(x)
+
+class Ridge(nn.Module,BaseLinearModel):
+    def __init__(self, in_features,out_features,alpha=1.0,useGradientDescent=False):
+        super().__init__()
+        self.alpha = alpha
+        self.linear = nn.Linear(in_features,out_features=out_features)
+        self.linear.weight.requires_grad = useGradientDescent
+        self.useGradientDescent = useGradientDescent
+
+    def fit(self, X:torch.Tensor, y):
+        if self.useGradientDescent:
+            raise Exception('if you wanna use gradient descent, please use torch.optim')
+        with torch.no_grad():
+            X=torch.cat([X,torch.ones(X.shape[0],1)],dim=1)
+            I=torch.eye(X.shape[1])
+            lambdaI=self.alpha*I
+            WB=torch.inverse(X.T@X+lambdaI)@X.T@y
+            self.linear.weight.data=WB[:-1]
+            self.linear.bias.data=WB[-1]
+            del WB,I,lambdaI
+            torch.cuda.empty_cache()
+
+    def loss(self, y_hat, y):
+        return F.mse_loss(y_hat, y) + self.alpha * F.mse_loss(self.linear.weight, torch.zeros_like(self.linear.weight))
+    
+    def forward(self, x):
+        return self.linear(x)
