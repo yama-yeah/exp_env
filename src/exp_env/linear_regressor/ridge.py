79874620ca83afc39a27706cf3b75675124f6af2
yama-yeah <b1021091@fun.ac.jp>
2024-04-22 22:46:14 +0900

    ridge回帰のFitを実装

--- src/exp_env/linear_regressor/ridge.py
+++ src/exp_env/linear_regressor/ridge.py
@@ -16,7 +16,6 @@ class LazyRidge(nn.Module,BaseLinearModel):
         if self.useGradientDescent:
             raise Exception('if you wanna use gradient descent, please use torch.optim')
         with torch.no_grad():
-            X=torch.cat([X,torch.ones(X.shape[0],1)],dim=1)
             I=torch.eye(X.shape[1])
             lambdaI=self.alpha*I
             WB=torch.inverse(X.T@X+lambdaI)@X.T@y
@@ -43,18 +42,14 @@ class Ridge(nn.Module,BaseLinearModel):
         if self.useGradientDescent:
             raise Exception('if you wanna use gradient descent, please use torch.optim')
         with torch.no_grad():
-            one=torch.ones(X.shape[0]*X.shape[1]).to(X.device).unsqueeze(-1)
-            X=X.flatten(end_dim=-2)
-            print(one.shape)
-            print(X.shape)
-            
+            one=torch.ones((X.shape[0],1)).to(X.device)
             X=torch.cat([X,one],dim=-1)
             I=torch.eye(X.shape[1]).to(X.device)
             lambdaI=self.alpha*I
             a=X.T@X+lambdaI
             WB=torch.inverse(a)@X.T
             WB=WB@y
-            self.linear.weight.data=WB[:-1]
+            self.linear.weight.data=WB[:-1].T
             self.linear.bias.data=WB[-1]
             del WB,I,lambdaI
             torch.cuda.empty_cache()
